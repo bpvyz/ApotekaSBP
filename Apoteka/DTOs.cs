ce575a164f9697305f2662736b213f2fb122806f
MarkoSt99 <121151055+MarkoSt99@users.noreply.github.com>
2024-04-25 23:31:20 +0200

    Izmene

--- Apoteka/DTOs.cs
+++ Apoteka/DTOs.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Apoteka.Entiteti;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,55 @@ using System.Threading.Tasks;
 
 namespace Apoteka
 {
-    internal class DTOs
+    #region Bolest
+    public class BolestBasic
     {
+        public int BolestId { get; set; }
+        public string Naziv { get; set; }
+
+        public virtual IList<LekLeciBasic> LekLeci { get; set; }
+        public virtual IList<LekKontraindikacijaBasic> LekKontraindikacija { get; set; }
+
+        public BolestBasic(int bolId, string Naziv)
+        {
+            this.BolestId = bolId;
+            this.Naziv = Naziv;
+
+        }
+
+        public BolestBasic()
+        {
+            LekLeci = new List<LekLeciBasic>();
+            LekKontraindikacija = new List<LekKontraindikacijaBasic>();
+        }
+    }
+
+    public class BolestPregled
+    {
+        public int BolestId { get; set; }
+        public string Naziv { get; set; }
+
+        public BolestPregled()
+        {
+        }
+
+        public BolestPregled(int bolId, string Naziv)
+        {
+            this.BolestId = bolId;
+            this.Naziv = Naziv;
+        }
+
+    }
+
+    #endregion
+
+    #region Farmaceut
+
+    public class FarmaceutBasic
+    {
+        public virtual string 
+
+
+
     }
 }
