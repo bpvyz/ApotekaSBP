771141a099fe15b6bfef186625ffda351840b148
paunzz <bogdanbokipaunovic@gmail.com>
2024-04-30 21:55:17 +0200

    Update dizajna, ovako treba da izgledaju ostatak formi. Obrati paznju na anchor, dock i podesavanje dugmeta (flat, paint itd)

--- Apoteka/Forme/ProdajnaMestaForm.cs
+++ Apoteka/Forme/ProdajnaMestaForm.cs
@@ -91,5 +91,14 @@ namespace Apoteka
             List<ProdajnoMestoPregled> podaci = DTOManager.vratiSvaProdajnaMesta();
             dataGridView1.DataSource = podaci;
         }
+
+        private void button1_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, button1.ClientRectangle,
+        SystemColors.ControlLightLight, 5, ButtonBorderStyle.Outset,
+        SystemColors.ControlLightLight, 5, ButtonBorderStyle.Outset,
+        SystemColors.ControlLightLight, 5, ButtonBorderStyle.Outset,
+        SystemColors.ControlLightLight, 5, ButtonBorderStyle.Outset);
+        }
     }
 }
